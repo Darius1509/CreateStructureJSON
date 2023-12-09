f4e983d13a63a27c608e537b59c7bd2b273130c9
Darius Dinu <dariusdinu19@gmail.com>
2023-12-09 16:24:07 +0200

    Implemented the main function(with checks for file paths)

---
+++ create_structure.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import json
+
+def create_structure(root_folder, structure):
+        print("to be implemented")
+        #TODO: Implementation
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python create_structure.py root_folder_path structure_json_file_path")
+        sys.exit(1)
+
+    root=sys.argv[1]
+    structure=sys.argv[2]
+
+    if not os.path.exists(structure):
+        print(f"Error: The JSON file '{structure}' does not exist.")
+        sys.exit(1)
+
+    with open(structure, 'r') as json_file:
+        structure = json.load(json_file)
+
+    if not os.path.exists(root):
+        os.makedirs(root)
+
+    create_structure(root, structure)
+    print("Folder structure created successfully.")
+
+if __name__ == "__main__":
+    main()
